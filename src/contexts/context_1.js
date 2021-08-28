bc2f97b2fd4ba5851383a5b9d875ffa37c12bd0d
mahmoud91 <mahmoudmheisen91@gmail.com>
2021-08-28 17:16:15 +0300

    create another context

---
+++ src/contexts/context_1.js
@@ -0,0 +1,61 @@
+import React, { useCallback, useReducer, createContext, useMemo } from "react";
+// import { useReducer } from "./contextDevTools";
+
+const SET_DATA_10 = "SET_DATA_10";
+const SET_DATA_11 = "SET_DATA_11";
+
+const initialState = {
+  data_10: false,
+  data_11: "",
+};
+
+const reducer = (state = initialState, action) => {
+  let { type, payload } = action;
+
+  switch (type) {
+    case SET_DATA_10:
+      return {
+        ...state,
+        data_10: !state.data_10,
+      };
+
+    case SET_DATA_11:
+      return {
+        ...state,
+        data_11: payload.data,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const Context1Provider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const setData10 = useCallback(() => {
+    dispatch({ type: SET_DATA_10 });
+  }, []);
+
+  const setData11 = useCallback(() => {
+    dispatch({ type: SET_DATA_11 });
+  }, []);
+
+  const context_1_value = useMemo(() => {
+    return {
+      state,
+      setData10,
+      setData11,
+    };
+  }, [state, setData10, setData11]);
+
+  return (
+    <Context1.Provider value={context_1_value}>{children}</Context1.Provider>
+  );
+};
+
+export const Context1 = createContext(initialState);
+
+export default Context1Provider;
+
+export { initialState, reducer };
